e7a983f79cf4d7b640b9dca63a702ccca0faf073
LucasGitHubx <lucasdarosa450@gmail.com>
2024-08-01 19:09:03 -0300

    Defining the store with zustand

---
+++ src/store/taskStore.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+import { Task } from "../types";
+
+interface State {
+  tasks: Task[];
+  setTask: (task: Task) => void;
+  deleteTask: (id: string) => void;
+  updateTask: (id: string, updatedTask: Task) => void;
+}
+
+export const useTaskStore = create<State>((set) => ({
+  tasks: [],
+  setTask: (task) =>
+    set((state) => ({
+      tasks: [...state.tasks, task],
+    })),
+  deleteTask: (id) =>
+    set((state) => ({
+      tasks: state.tasks.filter((task) => task.id !== id),
+    })),
+  updateTask: (id, updatedTask) =>
+    set((state) => ({
+      tasks: state.tasks.map((task) => (task.id === id ? updatedTask : task)),
+    })),
+}));
