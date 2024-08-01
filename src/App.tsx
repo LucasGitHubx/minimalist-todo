85706d64bd407dc2b058fd2be3afcf070f7668cc
LucasGitHubx <lucasdarosa450@gmail.com>
2024-08-01 13:42:50 -0300

    I created the App component

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import LoginPage from "./pages/LoginPage";
+
+export default function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={<MainLayout />} path="/todo/">
+        <Route element={<LoginPage />} path="/todo/login" />
+      </Route>
+    )
+  );
+
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
