da11e79cee5b67cc3f6e2b707106a8f31d8309b2
LucasGitHubx <lucasdarosa450@gmail.com>
2024-08-02 22:32:01 -0300

    Adding new route on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import {
 } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import LoginPage from "./pages/LoginPage";
+import MainPage from "./pages/MainPage";
 
 export default function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route element={<MainLayout />} path="/todo/">
+        <Route element={<MainPage />} index />
         <Route element={<LoginPage />} path="/todo/login" />
       </Route>
     )
